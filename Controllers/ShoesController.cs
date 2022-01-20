46a9f23313b89e2dfe02020a88452416ab891f2d
Marcos Bazbih <marcosbaza100@gmail.com>
2022-01-20 20:18:00 +0200

    shoes and clothing tables created

---
+++ Controllers/ShoesController.cs
@@ -0,0 +1,26 @@
+﻿using Marcos_Sport_Store.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Marcos_Sport_Store.Controllers
+{
+    public class ShoesController : Controller
+    {
+        static string connectionString = "Data Source=LENOVO-MARCOS;Initial Catalog=SportStoreDb;Integrated Security=True;Pooling=False";
+        SportStoreDataContext sportStoreDataContext = new SportStoreDataContext(connectionString);
+
+        // GET: Shoes
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetShoes()
+        {
+            return View(sportStoreDataContext.Shoes);
+        }
+    }
+}
