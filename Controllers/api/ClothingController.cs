7f864cace8614c9bfd43555dd0cdc422dd535103
Marcos Bazbih <marcosbaza100@gmail.com>
2022-01-26 20:32:33 +0200

    shoes and clothing api controllers added

---
+++ Controllers/api/ClothingController.cs
@@ -0,0 +1,129 @@
+﻿using Marcos_Sport_Store.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Marcos_Sport_Store.Controllers.api
+{
+    public class ClothingController : ApiController
+    {
+        static string connectionString = "Data Source=LENOVO-MARCOS;Initial Catalog=SportStoreDb;Integrated Security=True;Pooling=False";
+        SportStoreDataContext sportStoreDataContext = new SportStoreDataContext(connectionString);
+
+        public IHttpActionResult Get()
+        {
+            try
+            {
+                return Ok(sportStoreDataContext.Clothings.ToList());
+            }
+            catch (SqlException SqlEx)
+            {
+                return BadRequest(SqlEx.Message);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+
+        public IHttpActionResult Get(int id)
+        {
+            try
+            {
+                return Ok(sportStoreDataContext.Clothings.First((clothing) => clothing.Id == id));
+            }
+            catch (SqlException SqlEx)
+            {
+                return BadRequest(SqlEx.Message);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+
+        public IHttpActionResult Post([FromBody] Clothing value)
+        {
+            try
+            {
+                sportStoreDataContext.Clothings.InsertOnSubmit(value);
+                sportStoreDataContext.SubmitChanges();
+
+                return Ok("Item of Clothing has been added successfully");
+            }
+            catch (SqlException SqlEx)
+            {
+                return BadRequest(SqlEx.Message);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+
+        public IHttpActionResult Put(int id, [FromBody] Clothing value)
+        {
+            try
+            {
+                Clothing clothingToEdit = sportStoreDataContext.Clothings.First((Clothing) => Clothing.Id == id);
+
+                if (clothingToEdit != null)
+                {
+                    clothingToEdit.Type = value.Type;
+                    clothingToEdit.Gender = value.Gender;
+                    clothingToEdit.Company = value.Company;
+                    clothingToEdit.Model = value.Model;
+                    clothingToEdit.Price = value.Price;
+                    clothingToEdit.Quantity = value.Quantity;
+                    clothingToEdit.IsShort = value.IsShort;
+                    clothingToEdit.IsDryfit = value.IsDryfit;
+                    clothingToEdit.Image = value.Image;
+
+                    sportStoreDataContext.SubmitChanges();
+
+                    return Ok("Item of Clothing has been updated successfully");
+                }
+                return Ok("Sorry, No Item of Clothing was found");
+            }
+            catch (SqlException SqlEx)
+            {
+                return BadRequest(SqlEx.Message);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+
+        public IHttpActionResult Delete(int id)
+        {
+            try
+            {
+                sportStoreDataContext.Clothings.DeleteOnSubmit(sportStoreDataContext.Clothings.First((clothing) => clothing.Id == id));
+                sportStoreDataContext.SubmitChanges();
+
+                return Ok("Item of Clothing has been deleted successfully");
+            }
+            catch (SqlException SqlEx)
+            {
+                return BadRequest(SqlEx.Message);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ Controllers/api/ShoesController.cs
@@ -0,0 +1,127 @@
+﻿using Marcos_Sport_Store.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Marcos_Sport_Store.Controllers.api
+{
+    public class ShoesController : ApiController
+    {
+        static string connectionString = "Data Source=LENOVO-MARCOS;Initial Catalog=SportStoreDb;Integrated Security=True;Pooling=False";
+        SportStoreDataContext sportStoreDataContext = new SportStoreDataContext(connectionString);
+
+        public IHttpActionResult Get()
+        {
+            try
+            {
+                return Ok(sportStoreDataContext.Shoes.ToList());
+            }
+            catch (SqlException SqlEx)
+            {
+                return BadRequest(SqlEx.Message);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+
+        public IHttpActionResult Get(int id)
+        {
+            try
+            {
+                return Ok(sportStoreDataContext.Shoes.First((shoe) => shoe.Id == id));
+            }
+            catch (SqlException SqlEx)
+            {
+                return BadRequest(SqlEx.Message);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+
+        public IHttpActionResult Post([FromBody] Shoe value)
+        {
+            try
+            {
+                sportStoreDataContext.Shoes.InsertOnSubmit(value);
+                sportStoreDataContext.SubmitChanges();
+
+                return Ok("Shoe has been added successfully");
+            }
+            catch (SqlException SqlEx)
+            {
+                return BadRequest(SqlEx.Message);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+
+        public IHttpActionResult Put(int id, [FromBody] Shoe value)
+        {
+            try
+            {
+                Shoe shoeToEdit = sportStoreDataContext.Shoes.First((shoe) => shoe.Id == id);
+
+                if (shoeToEdit != null)
+                {
+                    shoeToEdit.Type = value.Type;
+                    shoeToEdit.Company = value.Company;
+                    shoeToEdit.Model = value.Model;
+                    shoeToEdit.Price = value.Price;
+                    shoeToEdit.Quantity = value.Quantity;
+                    shoeToEdit.InSale = value.InSale;
+                    shoeToEdit.Image = value.Image;
+
+                    sportStoreDataContext.SubmitChanges();
+
+                    return Ok("Shoe has been updated successfully");
+                }
+                return Ok("Sorry, No shoe was found");
+            }
+            catch (SqlException SqlEx)
+            {
+                return BadRequest(SqlEx.Message);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+
+        public IHttpActionResult Delete(int id)
+        {
+            try
+            {
+                sportStoreDataContext.Shoes.DeleteOnSubmit(sportStoreDataContext.Shoes.First((shoe) => shoe.Id == id));
+                sportStoreDataContext.SubmitChanges();
+
+                return Ok("Shoe has been deleted successfully");
+            }
+            catch (SqlException SqlEx)
+            {
+                return BadRequest(SqlEx.Message);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
