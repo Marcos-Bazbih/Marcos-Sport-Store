65d8494efc4dea2befbc8ca18d9af1bc217a35b7
Marcos Bazbih <marcosbaza100@gmail.com>
2022-01-28 20:51:17 +0200

    header and footer layout done. added js file and rearranged css layout

---
+++ Scripts/Main.js
@@ -0,0 +1,48 @@
+﻿const menu = document.getElementById("menu");
+const menuItem = document.getElementsByClassName("menuItem");
+const hamburger = document.getElementById("hamburger");
+
+const topLine = document.getElementById("topLine");
+const centerLine = document.getElementById("centerLine");
+const bottomLine = document.getElementById("bottomLine");
+
+let menuParam = false;
+
+function toggleMenu() {
+    if (menuParam == false) {
+        topLine.style.transform = "rotate(45deg)";
+        bottomLine.style.transform = "rotate(-45deg)";
+        centerLine.style.transform = "translateX(-150%)";
+
+        hamburger.style.borderColor = "white";
+        hamburger.style.background = "black";
+        topLine.style.background = "white";
+        bottomLine.style.background = "white";
+        centerLine.style.background = "white";
+
+        menuBox.style.transform = "translateX(0%)";
+
+        menuParam = true;
+    }
+    else {
+        topLine.style.transform = "rotate(0deg) translateY(-200%)";
+        bottomLine.style.transform = "rotate(0deg) translateY(200%)";
+        centerLine.style.transform = "translateX(0%)";
+
+        hamburger.style.borderColor = "black";
+        hamburger.style.background = "white";
+        topLine.style.background = "black";
+        bottomLine.style.background = "black";
+        centerLine.style.background = "black";
+
+        menuBox.style.transform = "translateX(-100%)";
+
+        menuParam = false;
+    }
+}
+
+hamburger.onclick = () => toggleMenu();
+
+for (const iterator of menuItem) {
+    iterator.onclick = () => toggleMenu();
+}
